40cee974168a357c7b660fd29bc420e58b29a2cb
Alex Rusin <rusin.ai@phystech.edu>
2022-10-07 22:00:59 +0300

    removed unnecessary state

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -3,17 +3,16 @@ import style from './Card.module.css'
 
 export default function Card({title, text, currentLikes}) {
     const [likes, changeLikes] = useState(currentLikes)
-    const [liked, setLiked] = useState(false);
-    const [likeStyle, setLikeStyle] = useState(style.notliked);
+    const [liked, setLiked] = useState(false)
 
+    const likeStyle = liked ? style.liked : style.notliked
     return (
         <div className={style.card}>
             <h1>{title}</h1>
             <div>{text}</div>
             <button
                 onClick={() => setLiked(() => {
-                    setLikeStyle(liked ? style.notliked : style.liked);
-                    changeLikes(likes + (liked ? -1 : 1));
+                    changeLikes(likes + (liked ? -1 : 1))
                     return !liked
                 })}
                 className={likeStyle + " " + style.likeButton}
