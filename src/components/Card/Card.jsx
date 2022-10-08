8325249941499d73478fa8369455efce96145d7b
Alex Rusin <rusin.ai@phystech.edu>
2022-10-08 22:20:44 +0300

    add comments
    I use not good a solution for rendering after removing a comment - `my_rerender`

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -4,12 +4,47 @@ import style from './Card.module.css'
 export default function Card({title, text, currentLikes}) {
     const [likes, changeLikes] = useState(currentLikes)
     const [liked, setLiked] = useState(false)
+    const [full, setFull] = useState(false)
+    const [re, my_rerender] = useState(true)
+    const [comments, updateComments] = useState(["Comment1", "Comment2", "Comment3"]);
 
     const likeStyle = liked ? style.liked : style.notliked
+    let style_card = style.card
+    let div_comments = <div></div>
+
+    if (full) {
+        style_card += " " + style.make_full
+
+        if (comments.length) {
+            div_comments = comments.map((comment, index) =>
+                <div className={style.comment} key={comment}>
+                    {comment}
+                    <button
+                        onClick={() => {
+                            comments.splice(index, 1)
+                            // updateComments(comments)
+                            my_rerender(!re)
+                        }}
+                        className={style.discard_comment}
+                    >
+                        <div>X</div>
+                    </button>
+                </div>
+            )
+        } else {
+            div_comments = <div className={style.comment}>
+                No comments
+            </div>
+        }
+    }
+
     return (
-        <div className={style.card}>
+        <div className={style_card}>
             <h1>{title}</h1>
-            <div>{text}</div>
+            <div className={style.text}>{text}</div>
+
+            {div_comments}
+
             <button
                 onClick={() => setLiked(() => {
                     changeLikes(likes + (liked ? -1 : 1))
@@ -17,9 +52,16 @@ export default function Card({title, text, currentLikes}) {
                 })}
                 className={likeStyle + " " + style.likeButton}
             >
-            <div>
                 <div>Likes: {likes}</div>
-            </div>
+            </button>
+
+            <button
+                onClick={() => {
+                    setFull(!full)
+                }}
+                className={style.commentsButton}
+            >
+                <div>{full ? "Hide comments" : "Open comments"}</div>
             </button>
         </div>
     )
