e1a172f405dad200d07fbf62502b0e07f4502363
Alex Rusin <rusin.ai@phystech.edu>
2022-11-13 15:24:37 +0300

    use functions

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,78 +1,65 @@
 import React, { useState } from "react";
 import style from './Card.module.css'
 
-export class Card extends React.Component  {
-    constructor(props) {
-        super(props);
-        this.state = {
-            likes: props.currentLikes,
-            liked: false,
-            full: false,
-            commentsCount: props.commentsCount,
-        };
-    }
+export function Card(props) {
+    const [likes, setLikes] = useState(props.currentLikes)
+    const [liked, setLiked] = useState(false)
+    const [full, setFull] = useState(false)
+    const [commentsCount, setCommentsCount] = useState(props.commentsCount)
 
-    render() {
-        const likeStyle = this.state.liked ? style.liked : style.notLiked
-        let styleCard = style.card
-        let divComments = <div></div>
+    const likeStyle = liked ? style.liked : style.notLiked
+    let styleCard = style.card
+    let divComments = <div></div>
 
-        if (this.state.full) {
-            styleCard += " " + style.makeFull
+    if (full) {
+        styleCard += " " + style.makeFull
 
-            if (this.state.commentsCount) {
-                divComments = this.props.comments.map((comment, index) =>
-                    <div className={style.comment}>
-                        <div className={style.comment_text}>{comment.text}</div>
-                        <div className={style.author}>by {comment.author}</div>
-                        <button
-                            onClick={() => {
-                                this.props.comments.splice(index, 1)
-                                this.setState({
-                                    commentsCount: this.state.commentsCount - 1
-                                })
-                            }}
-                            className={style.discardComment}
-                        >
-                            <div>X</div>
-                        </button>
-                    </div>
-                )
-            } else {
-                divComments = <div className={style.comment}>
-                    No comments
+        if (commentsCount) {
+            divComments = props.comments.map((comment, index) =>
+                <div className={style.comment}>
+                    <div className={style.comment_text}>{comment.text}</div>
+                    <div className={style.author}>by {comment.author}</div>
+                    <button
+                        onClick={() => {
+                            props.comments.splice(index, 1)
+                            setCommentsCount(commentsCount - 1)
+                        }}
+                        className={style.discardComment}
+                    >
+                        <div>X</div>
+                    </button>
                 </div>
-            }
-        }
-
-        return (
-            <div className={styleCard}>
-                <h1>{this.props.title}</h1>
-                <div className={style.text}>{this.props.text}</div>
-
-                {divComments}
-
-                <button
-                    onClick={() => this.setState({
-                        likes: this.state.likes + (this.state.liked ? -1 : 1),
-                        liked: !this.state.liked
-                    })}
-                    className={likeStyle + " " + style.likeButton}
-                >
-                    <div>Likes: {this.state.likes}</div>
-                </button>
-
-                <button
-                    onClick={() => {
-                        this.setState({
-                            full: !this.state.full
-                        })
-                    }}
-                    className={style.commentsButton}
-                >
-                    <div>{this.state.full ? "Hide comments" : "Open " + this.state.commentsCount + " comments"}</div>
-                </button>
+            )
+        } else {
+            divComments = <div className={style.comment}>
+                No comments
             </div>
-        )
+        }
     }
+
+    return (
+        <div className={styleCard}>
+            <h1>{props.title}</h1>
+            <div className={style.text}>{props.text}</div>
+
+            {divComments}
+
+            <button
+                onClick={() => setLiked(() => {
+                    setLikes(likes + (liked ? -1 : 1))
+                    return !liked
+                })}
+                className={likeStyle + " " + style.likeButton}
+            >
+                <div>Likes: {likes}</div>
+            </button>
+
+            <button
+                onClick={() => setFull(!full)}
+                className={style.commentsButton}
+            >
+                <div>{full ? "Hide comments" : "Open " + commentsCount + " comments"}</div>
+            </button>
+        </div>
+    )
 }
